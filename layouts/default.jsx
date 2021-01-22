810145bd6954b2add696282a5ce3d38ff1153760
Saveliy <seva007@hotmail.com>
2021-01-22 18:47:50 +0300

    Correct paddings

--- layouts/default.jsx
+++ layouts/default.jsx
@@ -3,7 +3,11 @@ import TheHeader from "../components/TheHeader/TheHeader";
 const DefaultLayout = ( { children } ) => {
     return <>
         <TheHeader />
-        { children }
+        <div className="defaultLayout">
+            <main>
+                { children }
+            </main>
+        </div>
     </>
 }
 
