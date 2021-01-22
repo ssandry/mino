297c2d78bbd3ea268b5b8ca4224bef0db6489851
Saveliy <seva007@hotmail.com>
2021-01-22 15:26:24 +0300

    Initial Mino
    Add styles for logo

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,8 +2,8 @@ import DefaultLayout from  "../layouts/default";
 
 const IndexPage = () => {
     return <DefaultLayout>
-        <h1>
-            MINO
+        <h1 className="logo">
+            MI<span>N</span>O
         </h1>
     </DefaultLayout>
 }
