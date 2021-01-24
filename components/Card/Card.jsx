3579c63b166a2e81980b3bfc656c4d607294fb96
Saveliy <seva007@hotmail.com>
2021-01-24 09:02:31 +0300

    Card component
    Add index  page
    Correct typography

---
+++ components/Card/Card.jsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+const Card = ({ HREF, SRC, ALT, engCL, jpCL, YEAR }) => {
+    return <Link href = {HREF} >
+        <a className="Card">
+            <div className="Card__img">
+                <img src={SRC} alt={ALT}/>
+            </div>
+            <div className="tags">
+                <div className="tags_line">
+                    <h6>{engCL} / <span>{jpCL}</span></h6> 
+                    <div className="tags_time">{YEAR}</div>
+                </div>
+            </div>
+        </a>
+    </Link>
+}
+
+export default Card;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,35 @@
 import DefaultLayout from  "../layouts/default";
+import Card from "../components/Card/Card";
 
 const IndexPage = () => {
     return <DefaultLayout>
-        <h1 className="logo">
-            MI<span>N</span>O
-        </h1>
+        <div className="headerIndex">
+            <h1 className="logo">
+                MI<span>N</span>O
+                Collections
+            </h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do tempor.
+            </p>
+        </div>
+        <div id="collections-grid">
+            <Card 
+                HREF="/"
+                SRC="/data/voile.jpg"
+                ALT=""
+                engCL="VOILE BLANCHE"
+                jpCL="ホワイトベ"
+                YEAR="2020"
+            />
+            <Card 
+                HREF="/"
+                SRC="/data/endless.jpg"
+                ALT=""
+                engCL="ENDLESS"
+                jpCL="ホワイト"
+                YEAR="2020"
+            />
+        </div>
     </DefaultLayout>
 }
 
