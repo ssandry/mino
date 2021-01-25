670af22ad2eeab8b0e103609d142c60d726c86d1
Saveliy <seva007@hotmail.com>
2021-01-25 10:04:31 +0300

    Correct Server

--- server/index.graphql.js
+++ server/index.graphql.js
@@ -2,22 +2,27 @@ const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const { buildSchema } = require("graphql");
 const { readFileSync } = require("fs");
+
 const SCHEMAGQL = readFileSync(`${__dirname}/schema.gql`, { encoding: "utf8" });
+
 const collections = require("../data/collections.json");
 
 const PORT = 3004;
 const SCHEMA = buildSchema(SCHEMAGQL);
 const app = express();
 
-const ROOT = {
+const RESOLVERS = {
     getAllCollections: () => {
         return collections
+    },
+    getCollection: (params) => {
+        return collections.find( ( {id} ) => { return id === params.id } )
     }
 }
 
 app.use("/", graphqlHTTP({
     schema: SCHEMA,
-    rootValue: ROOT,
+    rootValue: RESOLVERS,
     graphiql: true
 }))
 
