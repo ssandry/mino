14c6c665b22baacb650304b13cd0318f54540ff1
Saveliy <seva007@hotmail.com>
2021-01-26 17:01:49 +0300

    Add Footer Component

---
+++ components/TheFooter/index.jsx
@@ -0,0 +1,17 @@
+const TheFooter = () => {
+    return <>
+        <footer>
+
+        </footer>
+        <style jsx >
+            {`
+                footer {
+                    width: 100%;
+                    height: 150px;
+                }
+            `}
+        </style>
+    </>
+}
+
+export default TheFooter;
