862910cdee08851d646c00039ed4093bae2a9fab
Saveliy <seva007@hotmail.com>
2021-02-02 16:18:06 +0300

    Add static dir
    Add favicon

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -11,6 +11,7 @@ class DOC extends Document {
         return (
             <Html>
                 <Head>
+                    <link rel="shortcut icon" href="/static/favicon32x32.png" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap" rel="stylesheet" />
                     <link href="https://fonts.googleapis.com/css2?family=Bodoni+Moda:ital,wght@0,400;0,500;0,600;0,700;1,700&display=swap" rel="stylesheet" />
