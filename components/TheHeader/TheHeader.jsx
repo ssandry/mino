97c3e4ac567e9ac8482b38a213907ef85b6bbfaf
Saveliy <seva007@hotmail.com>
2021-02-08 18:42:28 +0300

    Activate Links

--- components/TheHeader/TheHeader.jsx
+++ components/TheHeader/TheHeader.jsx
@@ -1,8 +1,12 @@
-
 import Link from "next/link";
 import Logo from "../Logo";
 
+import { useState } from "react";
+
 const TheHeader = () => {
+
+    const [ acLn, setAcLn ] = useState("collections")
+
     return (
         <header>
             <div className="header">
@@ -17,17 +21,32 @@ const TheHeader = () => {
                     <ul>
                         <li>
                             <Link href="/">
-                                <a>Team</a>
+                                <a 
+                                    className={ acLn === "team" ? "active" : "disactive" }
+                                    onClick = { () => { setAcLn("team") } }
+                                >
+                                    Team
+                                </a>
                             </Link>
                         </li>
                         <li>
                             <Link href="/">
-                                <a>About</a>
+                                <a 
+                                    className={ acLn === "about" ? "active" : "disactive" }  
+                                    onClick = { () => { setAcLn("about") } }  
+                                >
+                                    About
+                                </a>
                             </Link>
                         </li>
                         <li>
                             <Link href="/">
-                                <a>Collections</a>
+                                <a 
+                                    className={ acLn === "collections" ? "active" : "disactive" }  
+                                    onClick = { () => { setAcLn("collections") } }  
+                                >
+                                    Collections
+                                </a>
                             </Link>
                         </li>
                     </ul>
